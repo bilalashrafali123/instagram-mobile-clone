ea49b1195741835b2b9dc18ed10301d2658be993
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-06-02 13:52:05 +0500

    working...

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.scss"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import "./index.scss"
+
+const Navbar = () => {
+  return (
+    <div className="container">
+     <div className="navbar"> 
+    <div className="white">instagram</div>
+    <div className="icons">
+    <i className="fa-regular fa-heart text-white"></i>
+    <i className="fa-regular fa-message text-white"></i>
+    </div>
+    </div>  
+    </div>
+  )
+}
+
+export default Navbar
